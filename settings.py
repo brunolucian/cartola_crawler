1f86d1d83f380c0f422b9b7a67b270cb4fa33b33
Evandro <dalbem.evandro@gmail.com>
2015-05-13 20:41:28 -0300

    arquivo com as configurações

---
+++ settings.py
@@ -0,0 +1,9 @@
+ # -*- coding: utf-8 -*-
+settings = {'cartola': {'url_login': 'https://login.globo.com/login/4728?tam=widget&url=https%3A%2F%2Fintervencao.globo.com%2Fintervencoes%2Fshow.do%3Fpopin%3Dtrue%26servicoId%3D4728%26urlIntervencao%3Dhttp%3A%2F%2Fs.glbimg.com%2Fgl%2Fba%2Fbarra-globocom.callback.html%2523http%253A%252F%252Fgloboesporte.globo.com%252Fcartola-fc%252F',
+                'email': '', 'pass': ''},
+            'mysql': {'host':'localhost', 'user':'',
+                'pass':'', 'db':'cartola'}}
+
+
+
+URL_MERCADO = 'http://cartolafc.globo.com/mercado/filtrar.json?page='
